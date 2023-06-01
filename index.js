b6a0db88e54c3a05e93a92aaa81c60a3ae41852f
algopert <yurisoeda1013@gmail.com>
2023-06-01 03:36:12 +0100

    add chatgpt

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ import { dirname } from 'path'
 import path from 'path'
 import route from './routes/routes.js'
 
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion() {
+    const completion = await openai.createCompletion({
+        model: "gpt-3.5-turbo-0301'",
+        prompt: "How are you today?",
+    });
+    console.log(completion.data.choices[0].text);
+}
+
+runCompletion();
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
